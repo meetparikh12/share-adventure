f9da7e97dbef112ba475947a9b1e642590baef62
meetparikh12 <parikhmeet1234@gmail.com>
2020-05-03 18:43:17 +0530

    Added different components for navigation

--- share-adventure-frontend/src/App.js
+++ share-adventure-frontend/src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import { BrowserRouter as Router, Route,Switch, Redirect} from 'react-router-dom';
 import Users from './users/pages/Users';
 import NewPlace from './places/pages/NewPlace';
-
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Users}/>
-        <Route exact path="/place/new" component={NewPlace}/>
-        <Redirect to="/"/>
-      </Switch>
-    </Router>
+      <Router>
+        <MainNavigation/>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Users}/>
+            <Route exact path="/place/new" component={NewPlace}/>
+            <Redirect to="/"/>
+          </Switch>
+        </main>
+      </Router>
   );
 }
 
