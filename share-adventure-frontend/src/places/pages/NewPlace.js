f9f5b6e1019c00910975cfeb088bdd1dfe62d7ce
meetparikh12 <parikhmeet1234@gmail.com>
2020-05-04 19:26:09 +0530

    Created NewPlace Component for adding new place

--- share-adventure-frontend/src/places/pages/NewPlace.js
+++ share-adventure-frontend/src/places/pages/NewPlace.js
@@ -1,14 +1,47 @@
 import React from 'react';
 
-import Input from '../../shared/components/FormElements/Input';
+// import Input from '../../shared/components/FormElements/Input';
 import './NewPlace.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class NewPlace extends React.Component {
+    constructor(props) {
+        super(props);
+        //this.handleSubmit = this.handleSubmit.bind(this);
+        this.fileInput = React.createRef();
+    }
+    render(){
+        return (
+           <div class="add-PBI">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 m-auto">
+                            {/* <a href="#" class="btn btn-light">
+                                Back to Project Board
+                            </a> */}
+                            <h4 class="display-4 text-center">Add New Place</h4>
+                            <form onSubmit={this.onSubmit}>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-lg" name="title" placeholder="Place Name" />
+                                </div>
+                                <div class="form-group">
+                                    <textarea class="form-control form-control-lg" placeholder="Place Description" name="description"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control form-control-lg" placeholder="Address" name="address"/>
+                                </div>
+                                <h6>Upload Image:</h6>
+                                <div class="form-group">
+                                    <input type="file" ref={this.fileInput} class="form-control form-control-lg" name="image" />
+                                </div>
+                                <input type="submit" class="btn btn-danger btn-block mt-4" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-const NewPlace = () => {
-  return (
-    <form className="place-form">
-      <Input element="input" type="text" label="Title" />
-    </form>
-  );
-};
+        )
+    };
+}
 
 export default NewPlace;
