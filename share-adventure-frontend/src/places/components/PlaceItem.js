d407576cdb960d1ff5fa423d5b42350eca5dff2b
meetparikh12 <parikhmeet1234@gmail.com>
2020-05-05 13:26:04 +0530

    Added delete method for deleting place

--- share-adventure-frontend/src/places/components/PlaceItem.js
+++ share-adventure-frontend/src/places/components/PlaceItem.js
@@ -1,31 +1,72 @@
-import React, { useState } from 'react';
+import React from 'react';
+ //{ useState } from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
-import Modal from '../../shared/components/UIElements/Modal';
+//import Modal from '../../shared/components/UIElements/Modal';
 import './PlaceItem.css';
 
 const PlaceItem = props => {
-  const [showMap, setShowMap] = useState(false);
+  //const [showMap, setShowMap] = useState(false);
+  //const [showConfirmModal, setShowConfirmModal] = useState(false);
 
-  const openMapHandler = () => setShowMap(true);
+    
+    const showDeleteWarningHandler = () => {
+      //setShowConfirmModal(true);
+      window.confirm('Do you want to delete this place? Please note that it cannot be undone.')
+    };
 
-  const closeMapHandler = () => setShowMap(false);
+    // const cancelDeleteHandler = () => {
+    //   setShowConfirmModal(false);
+    // };
+
+    // const confirmDeleteHandler = () => {
+    //   setShowConfirmModal(false);
+    //   console.log('DELETING...');
+    // };
+
+  const openMapHandler = () => {
+   // setShowMap(true);
+   alert('Sorry, Map can not be shown.');
+  }
+  //const closeMapHandler = () => setShowMap(false);
 
   return (
-    <React.Fragment>
-      <Modal
-        show={showMap}
-        onCancel={closeMapHandler}
-        header={props.address}
-        contentClass="place-item__modal-content"
-        footerClass="place-item__modal-actions"
-        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
-      >
-        <div className="map-container">
-          <h2>THE MAP!</h2>
-        </div>
-      </Modal>
+    // <React.Fragment>
+    //   <Modal
+    //     show={showMap}
+    //     onCancel={closeMapHandler}
+    //     header={props.address}
+    //     contentClass="place-item__modal-content"
+    //     footerClass="place-item__modal-actions"
+    //     footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+    //   >
+    //     <div className="map-container">
+    //       <h2>THE MAP!</h2>
+    //     </div>
+    //   </Modal>
+    //   <Modal
+    //     show={showConfirmModal}
+    //     onCancel={cancelDeleteHandler}
+    //     header="Are you sure?"
+    //     footerClass="place-item__modal-actions"
+    //     footer={
+    //       <React.Fragment>
+    //         <Button inverse onClick={cancelDeleteHandler}>
+    //           CANCEL
+    //         </Button>
+    //         <Button danger onClick={confirmDeleteHandler}>
+    //           DELETE
+    //         </Button>
+    //       </React.Fragment>
+    //     }
+    //   >
+    //     <p>
+    //       Do you want to delete this place? Please note that it
+    //       can't be undone thereafter.
+    //     </p>
+    //   </Modal>
+
       <li className="place-item">
         <Card className="place-item__content">
           <div className="place-item__image">
@@ -39,11 +80,10 @@ const PlaceItem = props => {
           <div className="place-item__actions">
             <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
             <Button to={`/place/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
           </div>
         </Card>
       </li>
-    </React.Fragment>
   );
 };
 
