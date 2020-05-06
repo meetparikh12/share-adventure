069e6f9e7b85be98d7f9c3acb153164900bb0ed7
meetparikh12 <parikhmeet1234@gmail.com>
2020-05-06 21:52:59 +0530

    Added middleware for unsupported routes

--- share-adventure-backend/app.js
+++ share-adventure-backend/app.js
@@ -3,12 +3,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const placesRoutes = require('./routes/places');
 const userRoutes = require('./routes/users');
-
+const ErrorHandling = require('./models/error-handling');
 app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', userRoutes);
 
+//Middleware for Unsupported Routes
+app.use((req,res,next)=> {
+    next(new ErrorHandling('The specified route does not exist', 404));
+})
+
+//Middleware for handling errors
 app.use((error,req,res,next)=> {
     const message = error.message || 'Unknown Error Occured';
     const status = error.statusCode || 500;
