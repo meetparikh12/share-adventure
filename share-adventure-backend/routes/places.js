5fa8856d64351f8ead0b4b72de7b2b6532e60131
meetparikh12 <parikhmeet1234@gmail.com>
2020-05-09 18:57:36 +0530

    Added auth middleware for making routes private

--- share-adventure-backend/routes/places.js
+++ share-adventure-backend/routes/places.js
@@ -16,11 +16,11 @@ route.post('/', auth, fileUpload.single('image') ,[
     //body('image').not().isEmpty().withMessage('Please provide an image')
 ], placeController.CREATE_NEW_PLACE);
 
-route.patch('/:placeId',[
+route.patch('/:placeId',auth, [
     body('title').trim().isLength({min: 5}).withMessage('Title should be minimum of 5 characters'),
     body('description').trim().isLength({min: 10}).withMessage('Description shoulf be minimum of 10 characters'),
 ], placeController.UPDATE_PLACE);
 
-route.delete('/:placeId', placeController.DELETE_PLACE);
+route.delete('/:placeId', auth, placeController.DELETE_PLACE);
 
 module.exports = route;
